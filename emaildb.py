236436bd04ab50aa6d526cf8c2b0c5f7ae7f3209
developer28hemanth0010 <75580491+developer28hemanth0010@users.noreply.github.com>
2021-08-14 20:23:49 +0530

    Add files via upload

---
+++ emaildb.py
@@ -0,0 +1,14 @@
+import sqlite3
+conn= sqlite3.connect('/Users/hemanthdatta/Codess/emails.db')
+c= conn.cursor()
+# c.execute("""CREATE TABLE email_list (
+#         exam text,
+#         email text
+#         )""")
+
+# c.execute("""INSERT INTO email_list VALUES('test1exam', 'test1email')
+#         """)
+
+# c.execute("""DELETE FROM email_list WHERE email = 'vallah@gmail.com'
+#         """)
+conn.commit()
