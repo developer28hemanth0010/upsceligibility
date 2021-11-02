4e1d79691ea832918652f60559bdc4416e7d1699
developer28hemanth0010 <developer.28hemanth0010@gmail.com>
2021-11-02 12:56:03 +0530

    check

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
+print("trial")
+
 
 @app.route("/")
 def main_page():
@@ -39,7 +41,6 @@ def getvalues():
                 ageover_error='Your entered age is out of attempts.'
                 return render_template("index.html",ageover_error=ageover_error)
             
-
 
         if exam=="National Defence Academy" and gender=="Male":
             main_return= calc('nda',input_dob)
