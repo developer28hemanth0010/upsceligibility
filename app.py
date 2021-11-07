f03feb6a77f32de5425a88c53419a56028ae457a
developer28hemanth0010 <developer.28hemanth0010@gmail.com>
2021-11-07 11:16:33 +0530

    use api

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask,render_template,request, jsonify
+from flask import Flask,render_template,request
+import requests
 
 app = Flask(__name__)
 
@@ -8,77 +9,22 @@ app = Flask(__name__)
 def main_page():
     return render_template('index.html')
 
-@app.route("/get", methods=['POST','GET'])
+@app.route("/response", methods=['POST','GET'])
 def getvalues():
 
     #importing values
     if request.method =='POST':
         exam= str(request.form['exam'])
         input_dob= str(request.form['input_dob'])
-        # category= str(request.form['category'])
         gender= str(request.form['gender'])
         preference= str(request.form['preference'])
-
-
-        def calc(table,dob):
-            import db_conn
-            results_list= db_conn.c.execute(f"""SELECT course from {table} WHERE starting_date <= '{dob}'
-                                        and ending_date >= '{dob}'
-                                    """)
-            results_list= list(db_conn.c.fetchall())
-            trialist= [i for ele in results_list for i in ele]
+        print(exam,input_dob,gender,preference)
     
-            if table=="cse" and len(results_list) :
-                results = ', '.join(str(v) for v in trialist)
-                return render_template("index.html", cse_results=results)
-
-            elif len(results_list):
-                results = ', '.join(str(v) for v in trialist)
-                return render_template("index.html", results=results)
-
-            else:
-                ageover_error='Your entered age is out of attempts.'
-                return render_template("index.html",ageover_error=ageover_error)
-            
-
-        if exam=="National Defence Academy" and gender=="Male":
-            main_return= calc('nda',input_dob)
-            return(main_return)
-
-        elif exam=="Combined Defence Services Examination" and gender=="Male" and (preference=="IMA" or preference=="INA"):
-            main_return= calc('cds_ima_ina',input_dob)
-            return(main_return)
-
-        elif exam=="Combined Defence Services Examination" and preference=="OTA":
-            main_return= calc('cds_ota_men_women',input_dob)
-            return(main_return)
-
-        elif exam=="AFCAT" and preference=="AFA_flying":
-            main_return= calc('afcat_fb',input_dob)
-            return(main_return)
-
-        elif exam=="AFCAT" and preference=="AFA_gd":
-            main_return= calc('afcat_gd',input_dob)
-            return(main_return)
-        
-        elif exam=="Combined Defence Services Examination" and gender=="Male" and preference=="AFA":
-            main_return= calc('cds_afa',input_dob)
-            return(main_return)
-
-        elif exam=="Civil Services Examination":
-            main_return= calc('cse',input_dob)
-            return ((main_return))
-
-        #ERRORS
-
-        elif exam=="Combined Defence Services Examination" and gender=="Female" and (preference=="IMA" or preference=="INA" or preference=="AFA"):
-            hello="Women are only eligibile to apply for OTA through CDS entry"
-            return render_template('index.html',ageover_error=hello)
-
-        else:
-            hello="An Error has occured, try back again after some time"
-            return render_template('index.html',ageover_error=hello)
-            
+        response = requests.get(f'https://upscapi.pythonanywhere.com/play?exam={exam}&gender={gender}&pref={preference}&dob={input_dob}')
+        response1= response.json()
+        response2= dict(response1)
+        response2=list(response2.values())[0]
+        return render_template('index.html',results=response2)
 
 
 if __name__=="__main__":
