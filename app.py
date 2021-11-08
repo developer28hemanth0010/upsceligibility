6cf6dd86d67b0182aee1be8943de3292618d1a6d
developer28hemanth0010 <75580491+developer28hemanth0010@users.noreply.github.com>
2021-11-08 19:54:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def getvalues():
         preference= str(request.form['preference'])
         print(exam,input_dob,gender,preference)
     
-        response = requests.get(f'https://upscapi.pythonanywhere.com/play?exam={exam}&gender={gender}&pref={preference}&dob={input_dob}')
+        response = requests.get(f'https://api.upsceligibility.live/play?exam={exam}&gender={gender}&pref={preference}&dob={input_dob}')
         response1= response.json()
         response2= dict(response1)
         response2=list(response2.values())[0]
